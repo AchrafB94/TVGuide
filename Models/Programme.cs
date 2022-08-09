7edb5e9baab2187ce1b830bccb89971e1ce62670
Achraf Bouanani <ashrafbouanani@gmail.com>
2022-08-09 10:02:06 +0100

    optimizations

--- Models/Programme.cs
+++ Models/Programme.cs
@@ -4,7 +4,7 @@
     {
         public DateTime Start { get; set; }
         public DateTime Stop { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Category { get; set; }
         public string? Image { get; set; }
--- Models/ProgrammeContext.cs
+++ Models/ProgrammeContext.cs
@@ -4,7 +4,7 @@ namespace TVGuide.Models
 {
     public class ProgrammeContext
     {
-        public static List<Programme>? list;
+        public static List<Programme> list;
 
         public static async Task Setup()
         {
@@ -14,8 +14,8 @@ namespace TVGuide.Models
 
             xmlSources.Add("https://iptv-org.github.io/epg/guides/eg-en/elcinema.com.epg.xml");
             xmlSources.Add("https://iptv-org.github.io/epg/guides/qa/bein.com.epg.xml");
-            xmlSources.Add("https://iptv-org.github.io/epg/guides/dz-ar/osn.com.epg.xml");
-            xmlSources.Add("https://iptv-org.github.io/epg/guides/fr/telecablesat.fr.epg.xml");
+            xmlSources.Add("https://iptv-org.github.io/epg/guides/dz-en/osn.com.epg.xml");
+            xmlSources.Add("https://iptv-org.github.io/epg/guides/fr/canalplus.com.epg.xml");
             xmlSources.Add("https://iptv-org.github.io/epg/guides/de/hd-plus.de.epg.xml");
             xmlSources.Add("https://iptv-org.github.io/epg/guides/it/mediaset.it.epg.xml");
             xmlSources.Add("https://iptv-org.github.io/epg/guides/it/raiplay.it.epg.xml");
@@ -43,11 +43,11 @@ namespace TVGuide.Models
              {
                  Start = Convert.ToDateTime($"{programme.Attribute("start").Value.Substring(0, 4)}-{programme.Attribute("start").Value.Substring(4, 2)}-{programme.Attribute("start").Value.Substring(6, 2)} {programme.Attribute("start").Value.Substring(8, 2)}:{programme.Attribute("start").Value.Substring(10, 2)}:00"),
                  Stop = Convert.ToDateTime($"{programme.Attribute("stop").Value.Substring(0, 4)}-{programme.Attribute("stop").Value.Substring(4, 2)}-{programme.Attribute("stop").Value.Substring(6, 2)} {programme.Attribute("stop").Value.Substring(8, 2)}:{programme.Attribute("stop").Value.Substring(10, 2)}:00"),
-                 Title = programme.Element("title").Value,
-                 Description = programme.Element("desc") != null ? programme.Element("desc").Value : string.Empty,
-                 Category = programme.Element("category") != null ? programme.Element("category").Value : string.Empty,
-                 Image = programme.Element("icon") != null ? programme.Element("icon").Attribute("src").Value : string.Empty,
-                 ChannelName = programme.Attribute("channel").Value
+                 Title = programme.Element("title")?.Value,
+                 Description = programme.Element("desc")?.Value,
+                 Category = programme.Element("category")?.Value,
+                 Image = programme.Element("icon")?.Attribute("src")?.Value,
+                 ChannelName = programme.Attribute("channel")?.Value
              }).ToList();
         }
 
