6d2dc889be82913215e3f801f4dae3c7e363ad1c
Achraf Bouanani <ashrafbouanani@gmail.com>
2022-08-28 22:07:17 +0100

    carousel

--- Models/ChannelRepository.cs
+++ Models/ChannelRepository.cs
@@ -91,7 +91,7 @@ public class ChannelRepository : IChannelRepository
         var rand = new Random();
         int toSkip = rand.Next(0, _context.Channels.Where(ch => ch.Package.Name == "Canal" || ch.Name.Contains("Rai")).Count());
         Channel randomChannel = _context.Channels.Where(ch => ch.Package.Name == "Canal" || ch.Name.Contains("Rai")).Skip(toSkip).Take(1).First();
-        var programmes = ProgrammeRepository.list.Where(prg => prg.ChannelName == randomChannel.IdXML && prg.Start >= DateTime.Today.AddHours(19)).Take(5).ToList();
+        var programmes = ProgrammeRepository.list.Where(prg => prg.ChannelName == randomChannel.IdXML && prg.Start >= DateTime.Today.AddHours(20)).Take(3).ToList();
         TonightViewModel model = new TonightViewModel();
         model.programs = programmes;
         model.channel = randomChannel;
