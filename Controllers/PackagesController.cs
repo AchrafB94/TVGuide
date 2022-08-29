01438c7e73bdfcc24c3093dac0c363190bbf8970
Achraf Bouanani <ashrafbouanani@gmail.com>
2022-08-29 12:21:57 +0100

    google auth button, package logos added.

--- Controllers/PackagesController.cs
+++ Controllers/PackagesController.cs
@@ -55,7 +55,7 @@ namespace TVGuide.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Package package)
+        public async Task<IActionResult> Create([Bind("Id,Name,Logo")] Package package)
         {
             if (ModelState.IsValid)
             {
@@ -87,7 +87,7 @@ namespace TVGuide.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Package package)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Logo")] Package package)
         {
             if (id != package.Id)
             {
